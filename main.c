523b77ce83a48d868853bd440029d2b9653eaca7
aditya <kirsaliaditya@gmail.com>
2023-10-20 12:56:35 +0530

    anges in argument list and negative numbers handled

--- main.c
+++ main.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "bigint.h"
+#include "BigInteger.h"
 int main()
 {
-    char *c1 = (char *) malloc(1000* sizeof(char));
-    char *c2 = (char *) malloc(1000* sizeof(char));
-    printf("enter two no:- ");
-    scanf("%s",c1);
-    getchar();
-    scanf("%s",c2);
-    printf("addition: ");
-    add(c1, c2);
-    printf("subtraction: ");
-    sub(c1, c2);
-    printf("multiplication: ");
-    mul(c1, c2);
-    printf("division: ");
-    divide(c1, c2);
+    struct BigInteger a = initialize("4567865456");
+    struct BigInteger b = initialize("2");
+    display(a);
+    display(b);
+    struct BigInteger c = add(a,b);
+    display(c);
+    c = sub(a,b);
+    display(c);
+    c= mul(a,b);
+    display(c);
+    c = div1(a,b);
+    display(c);
     return 0;
 }
